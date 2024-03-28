425eba334be8a7ed6f2f7e98b63adde85267db85
7e0240a4-b849-4250-80fc-180cafa9a2dd <fabio.gaiera@juliusbaer.com>
2024-03-28 14:25:40 +0100

    Python Interview

---
+++ 01 Week 2/source/03_diagonal_difference.py
@@ -0,0 +1,33 @@
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    length = len(arr)
+
+    # left to right diagonal
+    sum_left_right_diagonal = 0
+    i = 0
+    j = 0
+    while i <= length - 1:
+        sum_left_right_diagonal += arr[i][j]
+        i += 1
+        j += 1
+
+    # right to left diagonal
+    sum_right_left_diagonal = 0
+    i = 0
+    j = length - 1
+    while j >= 0:
+        sum_right_left_diagonal += arr[i][j]
+        i += 1
+        j -= 1
+
+    return abs(sum_left_right_diagonal - sum_right_left_diagonal)
+
+
+number = diagonalDifference([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(number)
