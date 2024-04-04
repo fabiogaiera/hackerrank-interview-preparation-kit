0971aa562add9117688fbdebf8c420a75d858447
fabiogaiera <fabio.gaiera@outlook.com>
2024-04-04 21:51:04 +0200

    Python Interview

---
+++ 02 Week 3/source/01_subarray_division_2.py
@@ -0,0 +1,15 @@
+def birthday(s, d, m):
+    ways = 0
+    for i in range(len(s)):
+        pass
+
+    return ways
+
+
+lst = [2, 2, 1, 3, 2]
+birth_day = 4
+birth_month = 2
+# result = birthday(lst, birth_day, birth_month)
+# print(result)
+
+print(2 % 0)
