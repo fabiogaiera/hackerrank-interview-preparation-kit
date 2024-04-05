563998339f16ca583266decbbd5b81b79ad963b4
7e0240a4-b849-4250-80fc-180cafa9a2dd <fabio.gaiera@juliusbaer.com>
2024-04-05 16:12:24 +0200

    Python Interview

--- 02 Week 3/source/01_subarray_division_2.py
+++ 02 Week 3/source/01_subarray_division_2.py
@@ -1,7 +1,16 @@
 def birthday(s, d, m):
     ways = 0
-    for i in range(len(s)):
-        pass
+    i = 0
+    while i < len(s):
+        suma = 0
+        j = i
+        while j < m and j < len(s):
+            suma += s[j]
+            j += 1
+        if suma == d:
+            ways += 1
+        m += m
+        i = j
 
     return ways
 
@@ -9,7 +18,5 @@ def birthday(s, d, m):
 lst = [2, 2, 1, 3, 2]
 birth_day = 4
 birth_month = 2
-# result = birthday(lst, birth_day, birth_month)
-# print(result)
-
-print(2 % 0)
+result = birthday(lst, birth_day, birth_month)
+print(result)
